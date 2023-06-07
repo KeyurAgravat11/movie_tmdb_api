68ec56fc4ddf32e9d7dd9ccf6367b4fb1bc65bdc
Mr.Ak <agravatk67@gmail.com>
2023-06-07 14:14:12 +0530

    Intial commit

---
+++ src/containers/App.test.tsx
@@ -0,0 +1,12 @@
+import { shallow } from "enzyme";
+import React from "react";
+import { App } from "./App";
+
+describe("App", () => {
+  it("should render", () => {
+    const getGenres = () => ({});
+    const component = shallow(<App getGenres={getGenres} />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
